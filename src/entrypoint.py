8ed89f70f495c925aa1152d132df541e29f96067
Fanni Jakó <fannijako@gmail.com>
2025-12-16 12:51:39 +0100

    add to entrypoint

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -3,6 +3,7 @@ import argparse
 
 from src.pink_board_part import PinkBoardPart
 from src.green_board_part import GreenBoardPart
+from src.blue_board_part import BlueBoardPart
 from src.die import Die, DieColor
 
 
@@ -12,24 +13,36 @@ def main() -> None:
     logging.info("args: %s", arguments)
     pink_board_part = PinkBoardPart()
     green_board_part = GreenBoardPart()
+    blue_board_part = BlueBoardPart()
 
-    for _ in range(10):
-        play_round(pink_board_part, green_board_part)
+    for _ in range(12):
+        play_round(pink_board_part, green_board_part, blue_board_part)
 
     print(pink_board_part)
     print(f'Pink board part score: {pink_board_part.evaluate()}')
     print(green_board_part)
     print(f'Green board part score: {green_board_part.evaluate()}')
+    print(blue_board_part)
+    print(f'Blue board part score: {blue_board_part.evaluate()}')
 
 
-def play_round(pink_board_part: PinkBoardPart, green_board_part: GreenBoardPart) -> None:
-    die = Die(DieColor.PINK)
-    die.roll()
-    pink_board_part.add_die(die)
+def play_round(pink_board_part: PinkBoardPart, green_board_part: GreenBoardPart, blue_board_part: BlueBoardPart) -> None:
+    pink_die = Die(DieColor.PINK)
+    pink_die.roll()
+    pink_board_part.add_die(pink_die)
 
-    die = Die(DieColor.GREEN)
-    die.roll()
-    green_board_part.add_die(die)
+    green_die = Die(DieColor.GREEN)
+    green_die.roll()
+    green_board_part.add_die(green_die)
+
+    blue_die = Die(DieColor.BLUE)
+    blue_die.roll()
+    white_die = Die(DieColor.WHITE)
+    white_die.roll()
+    try:
+        blue_board_part.add_die(blue_die, white_die)
+    except ValueError as e:
+        logging.warning(e)
 
 
 def parse_arguments() -> argparse.Namespace:
