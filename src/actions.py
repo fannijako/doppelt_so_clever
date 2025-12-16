5939e84e778cb9c0c5af4e9337056cddab7303cc
Fanni Jakó <fannijako@gmail.com>
2025-12-16 06:53:23 +0100

    actions

---
+++ src/actions.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class Action(Enum):
+    REROLL = "reroll"
+    REUSE = "reuse"
+    PLUS_ONE = "plus_one"
+    BLACK_QUESTION_MARK = "black_question_mark"
+    GREEN_QUESTION_MARK = "green_question_mark"
+    YELLOW_QUESTION_MARK = "yellow_question_mark"
+    BLUE_QUESTION_MARK = "blue_question_mark"
+    GREY_QUESTION_MARK = "grey_question_mark"
+    PINK_QUESTION_MARK = "pink_question_mark"
