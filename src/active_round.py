ff7e684bf81d4c1da942d42378b5f90e29959047
Fanni Jakó <fannijako@gmail.com>
2026-03-01 14:18:13 +0100

    add grey part to round

--- src/active_round.py
+++ src/active_round.py
@@ -49,7 +49,7 @@ class ActiveRound:  # pylint: disable=too-few-public-methods,too-many-instance-a
         logging.info(f'Picked color: {color}')
         return color
 
-    def pick_die(self) -> Optional[Dice]:
+    def pick_die(self) -> Optional[tuple[Dice, list[Dice]]]:
         color = self.pick_dice_color()
         dice = [dice for dice in self.available_die if str(dice.color.value) == color]
 
@@ -57,27 +57,41 @@ class ActiveRound:  # pylint: disable=too-few-public-methods,too-many-instance-a
         if not picked_dice:
             return None
         picked_number = picked_dice.value
+        smaller_die = [dice for dice in self.available_die if dice.value < picked_number]
         self.available_die.remove(picked_dice)
         self.picked_die.append(picked_dice)
-        self.discarded_die.extend(
-            [dice for dice in self.available_die
-             if dice.value < picked_number]
-        )
+        self.discarded_die.extend(smaller_die)
 
         logging.info(f"Picked die: {picked_dice}")
         logging.info(f"Available dice left: {[str(dice) for dice in self.available_die]}")
         logging.info(f"Discarded dice: {[str(dice) for dice in self.discarded_die]}")
-        return picked_dice
+        return picked_dice, smaller_die
 
     def execute(self):
         for game_round in range(1, 4):
             logging.info(f'Starting round {game_round}')
             self.roll_die()
 
-            picked_die = self.pick_die()
+            picked_die, smaller_die = self.pick_die()
             if picked_die.color == DiceColor.PINK:
                 self.board.pink_board_part.add_dice(picked_die)
             if picked_die.color == DiceColor.GREEN:
                 self.board.green_board_part.add_dice(picked_die)
             if picked_die.color == DiceColor.BLUE:
                 self.board.blue_board_part.add_dice(picked_die, self.white_dice)
+            if picked_die.color == DiceColor.GREY:
+                if DiceColor.WHITE in [dice.color for dice in smaller_die]:
+                    use_white_as = random.choice(
+                        [DiceColor.BLUE, DiceColor.GREEN, DiceColor.PINK, DiceColor.YELLOW]
+                    ) if self.automatic else input('Pick an available color to substitute white as: ')
+                else:
+                    use_white_as = None
+                use_grey_as = random.choice(
+                    [DiceColor.BLUE, DiceColor.GREEN, DiceColor.PINK, DiceColor.YELLOW]
+                    ) if self.automatic else input('Pick an available color to substitute grey as: ')
+                self.board.grey_board_part.add_dice(
+                    dice=picked_die,
+                    smaller_die=smaller_die,
+                    color_to_use_white_as=use_white_as,
+                    color_to_use_grey_as=use_grey_as
+                )
