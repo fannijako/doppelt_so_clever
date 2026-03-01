d9ed451002652f617ec185fd2f47761e92f89962
Fanni Jakó <fannijako@gmail.com>
2026-03-01 12:51:40 +0100

    grey box implementation

--- src/grey_board_part.py
+++ src/grey_board_part.py
@@ -1,9 +1,14 @@
 from src.grey_box import GreyBox
+from src.dice import DiceColor
 
 
 class GreyBoardPart:  # pylint: disable=too-few-public-methods
     def __init__(self):
-        self.boxes = [GreyBox() for _ in range(6)]
+        self.boxes = [
+            GreyBox(color=color, number=number)
+            for color in DiceColor
+            for number in range(1, 7)
+        ]
 
     def evaluate(self) -> int:
         return 0
--- src/grey_box.py
+++ src/grey_box.py
@@ -1,3 +1,39 @@
+import logging
+
+from src.dice import DiceColor
+
+
 class GreyBox:  # pylint: disable=too-few-public-methods
-    def __init__(self):
-        pass
+    def __init__(self, color: DiceColor, number: int) -> None:
+        logging.debug("Initializing a grey box")
+        self._validate_input(color, number)
+        self.color = color
+        self.number = number
+        self.is_crossed = False
+
+    @staticmethod
+    def _validate_input(color: DiceColor, number: int) -> None:
+        if not 1 <= number <= 6:
+            message = "Number must be between 1 and 6"
+            logging.error(message)
+            raise ValueError(message)
+
+        if color not in DiceColor:
+            valid_colors = [color.value for color in DiceColor]
+            message = "Color must be one of the following: " + ", ".join(valid_colors)
+            logging.error(message)
+            raise ValueError(message)
+
+    def cross_box(self, dice_color: DiceColor,  dice_value: int) -> None:
+        if dice_value == self.number and dice_color == self.color:
+            self.is_crossed = True
+            logging.info(f"Grey box for {self.color} | {self.number} crossed")
+            return
+        logging.info(
+            f"Dice value {dice_value} and dice color {dice_color} do not match "
+            f"for grey box {self.color} | {self.number}")
+
+    def __str__(self) -> str:
+        return (
+            f"Grey box: {self.color.value} | {self.number} | crossed: {self.is_crossed}"
+        )
