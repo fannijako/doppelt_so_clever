5be79fa1663030ff66c5106d7a989932d7990a23
Fanni Jakó <fannijako@gmail.com>
2026-03-07 12:30:57 +0100

    grey question mark

--- src/actions/immediate_actions/grey_question_mark.py
+++ src/actions/immediate_actions/grey_question_mark.py
@@ -1,6 +1,11 @@
-from src.actions.action_type import ActionType
-from src.actions.base_action import Action
+import random
+import logging
+
+from src.dice.dice import Dice
 from src.board.board import Board
+from src.dice.dice_color import DiceColor
+from src.actions.base_action import Action
+from src.actions.action_type import ActionType
 
 
 class GreyQuestionMarkAction(Action):
@@ -11,4 +16,30 @@ class GreyQuestionMarkAction(Action):
         raise ValueError("Action cannot be saved")
 
     def use(self, board: Board, automatic: bool) -> list[Action]:
-        return []
+        dice = Dice(DiceColor.GREY)
+        color, value = self._pick_color_and_value(automatic)
+        dice.set_value(value)
+
+        return board.grey_board_part.add_dice(
+            dice=dice,
+            smaller_die=[],
+            color_to_use_grey_as=color,
+        )
+
+    def _pick_color_and_value(self, automatic: bool) -> tuple[DiceColor, int]:
+        possible_combinations = [
+            (color, value)
+            for color in [
+                DiceColor.YELLOW,
+                DiceColor.BLUE,
+                DiceColor.BLUE,
+                DiceColor.PINK,
+            ]
+            for value in list(range(1, 7))
+        ]
+        if automatic:
+            return random.choice(possible_combinations)
+
+        logging.info(f"Possible combinations: {possible_combinations}")
+        index = int(input("Enter index: "))
+        return possible_combinations[index]
