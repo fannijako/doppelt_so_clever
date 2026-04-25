7270989fd9a0d7c21ec429da8d067e0dd2adcdaa
Fanni Jakó <fannijako@gmail.com>
2026-04-25 20:51:57 +0200

    always yellow

---
+++ src/input_handler/heuristics/always_yellow.py
@@ -0,0 +1,6 @@
+from src.input_handler.automatic_input_handler import AutomaticInputHandler
+
+
+class AlwaysYellowInputHandler(AutomaticInputHandler):
+    def __init__(self):
+        super().__init__()
