2fb5b7250ed5f3d809ae6dcdd38493c6c1b86958
Fanni Jakó <40933785+fannijako@users.noreply.github.com>
2026-04-25 21:08:27 +0200

    Heuristic based inputs (#20)

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -2,7 +2,8 @@ import argparse
 
 from src.game import Game
 from src.logging_config import setup_logging, GameLogger
-from src.input_handler import AutomaticInputHandler, ConsoleInputHandler
+from src.input_handler.heuristics.always_accept import AlwaysAcceptInputHandler
+from src.input_handler import InputHandler, AutomaticInputHandler, ConsoleInputHandler
 
 logger = GameLogger(__name__)
 
@@ -11,8 +12,7 @@ def main() -> None:
     arguments = parse_arguments()
     setup_logging(verbose=arguments.verbose)
     logger.info("Args", arguments)
-    input_handler = AutomaticInputHandler() if arguments.automatic else ConsoleInputHandler()
-    game = Game(input_handler=input_handler)
+    game = Game(input_handler=get_action_handler(arguments))
     game.play()
 
 
@@ -20,7 +20,16 @@ def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose logging")
     parser.add_argument("-a", "--automatic", action="store_true", help="Enable automatic play")
+    parser.add_argument("--always-accept", action="store_true", help="Use always-accept heuristic for automatic play")
     return parser.parse_args()
+
+
+def get_action_handler(arguments: argparse.Namespace) -> InputHandler:
+    if not arguments.automatic:
+        return ConsoleInputHandler()
+    if arguments.always_accept:
+        return AlwaysAcceptInputHandler()
+    return AutomaticInputHandler()
 
 
 if __name__ == "__main__":
---
+++ src/input_handler/heuristics/always_yellow.py
@@ -0,0 +1,5 @@
+from src.input_handler.automatic_input_handler import AutomaticInputHandler
+
+
+class AlwaysYellowInputHandler(AutomaticInputHandler):
+    pass
